5a686f56988ea4bcdc41ba4bb41c61b35154bef5
yaredghebre <yared2391@gmail.com>
2023-11-10 16:56:38 +0100

    rendered with map method (bonus complete)

--- controllers/posts.js
+++ controllers/posts.js
@@ -5,17 +5,37 @@ const index = (req, res) => {
   // uso format per abbreviare la content negotiation
   res.format({
     html: () => {
-      const htmlPosts = ["<h1>Feed</h1>"];
+      // Usando il CICLO FOR OF
+      //   const htmlPosts = ["<h1>Feed</h1>"];
 
-      htmlPosts.push("<ul>");
+      //   htmlPosts.push("<ul>");
 
-      for (const post of feed) {
-        htmlPosts.push(`<li>
-        <h3>${post.titolo}</h3>
-        <img src="/img/posts/${post.immagine}"
-        </li>`);
-      }
-      htmlPosts.push("</ul>");
+      //   for (const post of feed) {
+      //     htmlPosts.push(`<li>
+      //     <h3>${post.titolo}</h3>
+      //     <img src="/img/posts/${post.immagine}
+      //     </li>`);
+      //   }
+      //   htmlPosts.push("</ul>");
+
+      // Usando il METODO MAP
+      const htmlPosts = [
+        "<h1>Feed</h1>",
+        "<ul>",
+        ...feed.map(
+          (post) => `<li>
+            <h3>${post.titolo}</h3>
+            <img src="/img/posts/${post.immagine}" alt="${post.titolo}" />
+            <ul>
+              <li>${post.contenuto}</li>
+              <li>
+                <ol>${post.tags.map((tag) => `<li>${tag}</li>`).join("")}</ol>
+              </li>
+            </ul>
+          </li>`
+        ),
+        "</ul>",
+      ];
 
       // send anziche end
       res.type("html").send(htmlPosts.join(""));
